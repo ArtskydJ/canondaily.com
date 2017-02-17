490698e5d591647429bae7822fcea9f2e9e9e07a
Joseph Dykstra <josephdykstra@gmail.com>
2017-02-16 23:30:10 -0600

    Add transform-bookmarks.js and output.json

---
+++ transform-bookmarks.js
@@ -0,0 +1,33 @@
+const fs = require('fs')
+
+const bookmarksTxt = fs.readFileSync('./bookmarks.txt', 'utf-8')
+
+var result = bookmarksTxt.split('\n').reduce(function (memo, line) {
+	if (line.startsWith('\t\t\t')) { // passage
+		var dayString = memo.state.month + ' ' + memo.state.dayOfMonth
+		if (!memo.result[dayString]) {
+			memo.result[dayString] = []
+		}
+		var passage = memo.state.book + ' ' + line.trim()
+		memo.result[dayString].push(passage)
+		memo.state.dayOfMonth++
+	} else if (line.startsWith('\t\t')) { // book
+		memo.state.book = line.trim()
+	} else if (line.startsWith('\t')) { // month
+		memo.state.dayOfMonth = 1
+		memo.state.month = line.trim()
+	}
+	return memo
+}, {
+	state: {
+		month: null,
+		book: null,
+		dayOfMonth: 1
+	},
+	result: {
+		// 'January 1': [ 'Genesis 1-2', 'Psalms 1', 'Matthew 1:1-17', 'Acts 1:1-11' ]
+	}
+}).result
+
+// console.log(result)
+fs.writeFileSync('./output.json', JSON.stringify(result))
