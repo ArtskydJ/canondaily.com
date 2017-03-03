5871fd97d66ebe8b517922b17078d29049cdbaf7
Joseph Dykstra <josephdykstra@gmail.com>
2017-03-02 23:00:31 -0600

    Fix #2, #3, #4, #5

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ if (!dayToPassagesMap) throw new Error('uh oh......')
 
 var now = new Date()
 var dateInput = document.getElementById('date')
+var buttons = Array.prototype.slice.call(document.querySelectorAll('.nav-button'))
+var iframes = Array.prototype.slice.call(document.querySelectorAll('iframe'))
 var qsDate = /[?&]date=(\d{4}-\d\d-\d\d)/.exec(location.search)
 if (qsDate) {
 	now = new Date(qsDate[1])
@@ -12,37 +14,37 @@ dateInput.value = now.toISOString().slice(0, 10)
 
 var today = (now.getUTCMonth() + 1) + '/' + now.getUTCDate()
 var passages = dayToPassagesMap[today]
-var passageIndex = 0
+var passageIndex = -1
 
-function setPassage(offset) {
-	var prev = document.querySelector('.previous')
-	var next = document.querySelector('.next')
-	var iframeEle = document.getElementById('iframe')
-	var passageEle = document.getElementById('passage')
-
+function init() {
 	if (passages) {
-		var nextPassageIndex = Math.min(3, Math.max(0, passageIndex + offset))
-		if (offset === 0 || nextPassageIndex !== passageIndex) {
-			passageIndex = nextPassageIndex
-			var passage = passages[passageIndex]
-			passageEle.innerHTML = 'Bookmark ' + (passageIndex + 1)
-			document.title = 'Bible In A Year - ' + today + ' - ' + passage
-			iframeEle.src = 'https://www.biblegateway.com/passage/?version=NKJV&interface=print&search=' + encodeURIComponent(passage)
-		}
-
-		prev.classList.remove('disabled')
-		next.classList.remove('disabled')
-		if (passageIndex === 0) prev.classList.add('disabled')
-		if (passageIndex === 3) next.classList.add('disabled')
-	} else {
-		passageEle.innerHTML = 'No passage found!'
-		document.title = 'Bible In A Year - ' + today + ' - No passage found!'
-
-		iframeEle.srcdoc = '<h1 style="font-family:Calibri,sans-serif;text-align:center;width:calc(100vw - 8px);position:absolute;top:45vh;">No passage found!</h1>'
-
-		prev.classList.add('disabled')
-		next.classList.add('disabled')
+		iframes.forEach(function (iframe, index) {
+			iframe.src = 'https://www.biblegateway.com/passage/?version=NKJV&interface=print&search=' + encodeURIComponent(passages[index])
+		})
 	}
 }
 
+function setPassage(nextPassageIndex) {
+	if (passages && nextPassageIndex !== passageIndex) {
+		passageIndex = nextPassageIndex
+		var passage = passages[passageIndex]
+		document.title = 'Bible In A Year - ' + today + ' - ' + passage
+
+		buttons.forEach(function (button) { button.classList.remove('disabled') })
+		buttons[passageIndex].classList.add('disabled')
+
+		iframes.forEach(function (iframe) { iframe.classList.remove('front') })
+		iframes[passageIndex].classList.add('front')
+
+	} else if (!passages) {
+		document.title = 'Bible In A Year - ' + today + ' - No passage found!'
+
+		iframes[0].srcdoc = '<h1 style="font-family:Calibri,sans-serif;text-align:center;width:calc(100vw - 8px);position:absolute;top:45vh;">No passage found!</h1>'
+		iframes[0].classList.add('front')
+
+		buttons.forEach(function (ele) { ele.classList.add('disabled') })
+	}
+}
+
+init()
 setPassage(0)
