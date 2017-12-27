0e31af41464d0526545df90a801f13a6ff98465b
Joseph Dykstra <josephdykstra@gmail.com>
2017-12-26 23:47:05 -0600

    Build to main.js instead of dayToPassagesMap.js

--- _original-bookmarks/transform-bookmarks.js
+++ _original-bookmarks/transform-bookmarks.js
@@ -1,8 +1,23 @@
 const fs = require('fs')
 const parse = require('./parser.js')
 const bookmarksTxt = fs.readFileSync(__dirname + '/bookmarks.txt', 'utf-8')
+const mainJs = fs.readFileSync(__dirname + '/../main.js', 'utf-8')
 
 var result = parse(bookmarksTxt)
+var json = JSON.stringify(result)
+	.replace(/\],/g, '],\n\t\t')
+	.replace('{"', '{\n\t\t"')
+	.replace(']}', ']\n\t}')
 
-const newIndexHtml = 'window.dayToPassagesMap = \n' + JSON.stringify(result).replace(/\],/g, '],\n')
-fs.writeFileSync(__dirname + '/../dayToPassagesMap.js', newIndexHtml)
+const unchangingMainJsCode = mainJs.replace(/\/\* CHANGES BELOW THIS LINE WILL BE OVERWRITTEN[\s\S]+$/, '')
+const newMainJs = unchangingMainJsCode +
+`/* CHANGES BELOW THIS LINE WILL BE OVERWRITTEN
+Make changes to bible-in-a-year/_original-bookmarks/bookmarks.txt */
+
+function getPassages(day) {
+	var dayToPassagesMap = ${json}
+	return dayToPassagesMap[day]
+}
+`
+
+fs.writeFileSync(__dirname + '/../main.js', newMainJs)
