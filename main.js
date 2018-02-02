5cba03935beed66be6280f87cb6420ecb3c0acda
Joseph Dykstra <josephdykstra@gmail.com>
2018-02-02 00:57:15 -0600

    Fix date handling and improve error handling

--- main.js
+++ main.js
@@ -1,30 +1,37 @@
 (function () {
 
 	var dateInput = document.getElementById('date')
-	var now = new Date()
+	var day = new Date()
+	var today = ''
+
 	var qsDate = /[?&]date=(\d{4}-\d\d-\d\d)/.exec(location.search)
 	if (qsDate) {
-		now = new Date(qsDate[1])
+		day = new Date(qsDate[1])
+		dateInput.value = qsDate[1]
+		today = pad(day.getUTCMonth() + 1) + '/' + pad(day.getUTCDate())
+	} else {
+		dateInput.value = getLocalISODateString(day)
+		today = pad(day.getMonth() + 1) + '/' + pad(day.getDate())
 	}
-	dateInput.value = getLocalISODateString(now)
 
-	var today = pad(now.getMonth() + 1) + '/' + pad(now.getDate())
 	document.title = 'Bible In A Year - ' + today
 
 	xhr('./passages/' + today + '.html', function (err, result) {
+		var contentHtml = ''
 		if (err) {
-			alert(err.message)
-			throw err
+			document.getElementById('background-message').innerHTML = err.message
+		} else if (!result.ok) {
+			document.getElementById('background-message').innerHTML = result.status + ' ' + result.statusText
+		} else {
+			document.getElementById('content').innerHTML = result.text
 		}
-
-		document.getElementById('content').innerHTML = result.text
 	})
 
 
 	function getLocalISODateString(date) {
-		var year = now.getFullYear()
-		var month = pad(now.getMonth() + 1)
-		var day = pad(now.getDate())
+		var year = date.getFullYear()
+		var month = pad(date.getMonth() + 1)
+		var day = pad(date.getDate())
 		return year + '-' + month + '-' + day
 	}
 
