a91233b2d8d3c7265d6f6ca1f5ec02aeec708a7e
Joseph Dykstra <josephdykstra@gmail.com>
2019-02-28 23:18:45 -0600

    Improve the calendar
    Make it responsive
    Add a Today button at the top; Fixes #31
    Space it out, so Sunday is on the left, and Saturday is on the right

--- _generator/generate-calendar.js
+++ _generator/generate-calendar.js
@@ -1,18 +1,44 @@
-const monthNames = [,'January','February','March','April','May','June',
+var monthNames = [,'January','February','March','April','May','June',
 	'July','August','September','October','November','December']
+const shortMonthNames = [,'Jan','Feb','Mar','Apr','May','June',
+	'July','Aug','Sep','Oct','Nov','Dec']
 var expectedMonthLength = [,31,28,31,30,31,30,31,31,30,31,30,31]
 
+// The calendar needs to be re-generated yearly.
 module.exports = function generateCalendar(year) { // year e.g. 2019
-	var jan1ms = new Date(year, 0)
-	jan1ms.setUTCHours(0)
+	var jan1 = new Date(year, 0)
+	jan1.setUTCHours(0)
+	var dayOfWeek = (jan1.getDay() + 1) % 7
 
 	var resultHtml = ''
+
+	resultHtml += '<a class="today" href="">Today</a>'
+	if (dayOfWeek) {
+		resultHtml += `<div class="date-spacer" style="grid-column-end:${dayOfWeek + 1};"></div>`
+	}
+
 	for (var month = 1; month <= 12; month++) {
 		for (var day = 1; day <= expectedMonthLength[month]; day++) {
-			resultHtml += `<a class="date" href="/${monthNames[month]}/${day}">${monthNames[month]} ${day}
-				<div class="checkmark"></div>
+			resultHtml += `<a class="date" href="/${monthNames[month]}/${day}">
+				${shortMonthNames[month]}<br>${day}
 			</a>`
+				//<div class="checkmark"></div>
 		}
 	}
-	return `<div id="calendar">${resultHtml}</div>`
+	return `<div id="calendar">${resultHtml}</div>
+		<script>
+			var monthNames = [,'January','February','March','April','May','June',
+				'July','August','September','October','November','December']
+
+			function updateToday() {
+				var eleToday = document.querySelector('a.today')
+				var d = new Date().toLocaleDateString('en-US', { month: 'long', day: 'numeric' })
+				eleToday.href = '/' + d.replace(' ', '/')
+			}
+
+			updateToday()
+
+			setInterval(updateToday, 1000)
+		</script>
+	`
 }
