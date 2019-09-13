c69d2321df6eaf8634a608cca7800bdbc56bcc1b
Joseph Dykstra <josephdykstra@gmail.com>
2019-09-13 01:18:49 -0500

    Add context menu toggle for completed on main calendar page

--- _generator/generate-calendar.js
+++ _generator/generate-calendar.js
@@ -17,7 +17,7 @@ module.exports = function generateCalendar(year) { // year e.g. 2019
 		for (var day = 1; day <= expectedMonthLength[month]; day++) {
 			resultHtml += `<a class="cal-date" href="/${monthNames[month]}/${day}" data-month="${month}" data-day="${day}">
 				${shortMonthNames[month]}<br>${day}
-			</a>`.replace(/\s+/g, ' ')
+			</a>`.replace(/\s+/g, ' ') + '\n'
 				//<div class="checkmark"></div>
 		}
 	}
@@ -45,6 +45,36 @@ module.exports = function generateCalendar(year) { // year e.g. 2019
 					dateEle.classList.add('completed')
 				}
 			})
+
+			document.getElementById('calendar').oncontextmenu = function (ev) {
+				var dateEle = ev.target
+				//console.log('ev', ev)
+				if (dateEle.classList.contains('cal-date')) {
+					ev.preventDefault()
+					ev.stopPropagation()
+					var completed = dateEle.classList.contains('completed')
+					markDayAs(dateEle.dataset.month, dateEle.dataset.day, !completed)
+					if (completed) {
+						dateEle.classList.remove('completed')
+					} else {
+						dateEle.classList.add('completed')
+					}
+				}
+			}
+
+			function readSavedMonth(month) {
+				return JSON.parse(localStorage.getItem(month) || '[]')
+			}
+			function writeSavedMonth(month, monthData) {
+				localStorage.setItem(month, JSON.stringify(monthData))
+			}
+
+			function markDayAs(month, day, complete){
+				var monthData = readSavedMonth(month)
+				var wasComplete = monthData[day] === 1
+				monthData[day] = complete ? 1 : 0
+				writeSavedMonth(month, monthData)
+			}
 		</script>
 	`
 }
