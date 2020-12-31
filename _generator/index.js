caf89897f6f8fc8d4c49c309ede36c8dcb01bc3c
ArtskydJ <josephdykstra@gmail.com>
2020-12-31 11:06:24 -0600

    Follow FP guidelines for the generator

--- _generator/index.js
+++ _generator/index.js
@@ -23,31 +23,33 @@ const dtpm = parseBookmarks(bookmarksTxt)
 
 const { monthNames, expectedMonthLength, shortMonthNames } = require('./constant/months.json')
 
+const revHash = hashCss()
+const year = new Date().getUTCFullYear()
 
 if (cliOpts.debug) {
-	generateCommonFiles()
+	generateCommonFiles(revHash, year)
 
-	generateDayHtml(1, 1)
-	generateDayHtml(1, 2)
-	generateDayHtml(1, 3)
+	generateDayHtml(revHash, 1, 1)
+	generateDayHtml(revHash, 1, 2)
+	generateDayHtml(revHash, 1, 3)
 	console.log('\nSkipping the daily pages other than Jan 1,2,3')
 	console.log('Open localcanondaily.com in your browser.\n')
 } else if (cliOpts.month) {
-	generateCommonFiles()
+	generateCommonFiles(revHash, year)
 
 	const mMonth = parseInt(cliOpts.month, 10)
 	if (mMonth < 1 || mMonth > 12) throw new RangeError('Unexpected value for month cli argument')
 
 	for (let mDay = 1; mDay <= expectedMonthLength[mMonth]; mDay++) {
-		generateDayHtml(mMonth, mDay)
+		generateDayHtml(revHash, mMonth, mDay)
 	}
 	console.log('\nOnly generating for month ' + mMonth)
 } else if (cliOpts.run) {
-	generateCommonFiles()
+	generateCommonFiles(revHash, year)
 
 	for (let rMonth = 1; rMonth <= 12; rMonth++) {
 		for (let rDay = 1; rDay <= expectedMonthLength[rMonth]; rDay++) {
-			generateDayHtml(rMonth, rDay)
+			generateDayHtml(revHash, rMonth, rDay)
 		}
 	}
 } else {
@@ -63,10 +65,7 @@ function printUsage() {
 	console.log('node . month=n     Generate for a month. n must be between 1 and 12')
 }
 
-function generateCommonFiles() {
-	const revHash = hashCss()
-	global.revHash = revHash
-
+function generateCommonFiles(revHash, year) {
 	writeSubtemplate('index.html', {
 		subtemplate: './calendar.art',
 		title: 'Canon Daily',
@@ -76,8 +75,8 @@ function generateCommonFiles() {
 		monthNames,
 		monthNamesJson: JSON.stringify(monthNames),
 		shortMonthNames,
-		dayOfWeek: getDayOfWeekOffset(),
-		leapYear: getLeapYear(),
+		dayOfWeek: getDayOfWeekOffset(year),
+		leapYear: getLeapYear(year),
 	})
 
 	writeSubtemplate('prayer-for-filling-of-spirit.html', {
@@ -87,7 +86,7 @@ function generateCommonFiles() {
 	})
 }
 
-function generateDayHtml(month, day) {
+function generateDayHtml(revHash, month, day) {
 	let backUrl = `./${ day - 1 }`
 	if (month === 1 && day === 1) {
 		backUrl = '../December/31'
@@ -104,7 +103,7 @@ function generateDayHtml(month, day) {
 	writeSubtemplate(monthNames[month] + '/' + day + '.html', {
 		subtemplate: './day.art',
 		title: `${ monthNames[month] } ${ day } - Canon Daily`,
-		revHash: global.revHash,
+		revHash,
 		month,
 		day,
 		proudVsBroken: proudVsBroken[day - 1],
@@ -136,15 +135,13 @@ function range(start, end) {
 	return Array(end - start + 1).fill().map((_, i) => i + start) // *new* Array?
 }
 
-function getDayOfWeekOffset() {
-	const year = new Date().getUTCFullYear()
+function getDayOfWeekOffset(year) {
 	const jan1 = new Date(year, 0)
 	jan1.setUTCHours(0)
 	return (jan1.getDay() + 1) % 7
 }
 
-function getLeapYear() {
-	const year = new Date().getUTCFullYear()
+function getLeapYear(year) {
 	// https://stackoverflow.com/a/16353241/1509389
 	return ((year % 4 === 0) && (year % 100 !== 0)) || (year % 400 === 0)
 }
